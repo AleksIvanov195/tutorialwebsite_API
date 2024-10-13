de4c610394675809bfa562affd9417174a4e28cd
AleksIvanov195 <alexsandar196@gmail.com>
2024-10-13 18:13:19 +0100

    feat: Default router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import userCourseRoutes from './routes/usercourseRoutes.js';
 import courseRoutes from './routes/courseRoutes.js';
+import { domainRouter, defaultRouter } from './routes/defaultRouter.js';
 const app = new express();
 
 // parsing JSON bodies
@@ -9,9 +10,11 @@ app.use(express.json());
 
 // Routes
 
-app.use('/users', userRoutes);
-app.use('/usercourses', userCourseRoutes);
-app.use('/courses', courseRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/usercourses', userCourseRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/', domainRouter);
+app.use('/api/*', defaultRouter);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
---
+++ routes/defaultRouter.js
@@ -0,0 +1,82 @@
+import express from 'express';
+
+const router = express.Router();
+const API_URL = 'http://localhost:3000/api';
+
+const listOfEndpoints = [
+	{
+		entity: 'Course',
+		sap: '/api/courses',
+		services: [
+			{
+				endpoint: '/',
+				description: 'Return all courses',
+				examples: [`${API_URL}/courses`],
+			},
+			{
+				endpoint: '/:courseID',
+				description: 'Return a course by ID',
+				examples: [`${API_URL}/courses/1`],
+			},
+			{
+				endpoint: '/user/:userID/statuses',
+				description: 'Return all courses with a status for the specified user',
+				examples: [`${API_URL}/courses/user/1/statuses`],
+			},
+		],
+	},
+	{
+		entity: 'UserCourse',
+		sap: '/api/usercourses',
+		services: [
+			{
+				endpoint: '/',
+				description: 'Return all usercourses interactions for a specific user',
+				examples: [`${API_URL}/usercourses/user/1`],
+			},
+			{
+				endpoint: '/:usercourseID',
+				description: 'Return a usercourse interaction',
+				examples: [`${API_URL}/usercourses/777`],
+			},
+			{
+				endpoint: '/user/:userID/status/:statusID?',
+				description: 'Return a specific user\'s usercourse interactions with an optional statusID',
+				examples: [
+					`${API_URL}/usercourses/user/1`,
+					`${API_URL}/usercourses/user/1/status/1`,
+				],
+			},
+		],
+	},
+	{
+		entity: 'User',
+		sap: '/api/users',
+		services: [
+			{
+				endpoint: '/:id',
+				description: 'Return a user by ID',
+				examples: [`${API_URL}/users/1`],
+			},
+		],
+	},
+];
+
+
+router.get('/', (req, res) => {
+	res.status(200).json({
+		message: 'List of available endpoints',
+		listOfEndpoints,
+	});
+});
+
+const defaultRouter = express.Router();
+
+defaultRouter.get('/', (req, res) => {
+	res.status(404).json({
+		message: 'Specified endpoint not found',
+		listOfEndpoints,
+	});
+});
+
+export { router as domainRouter, defaultRouter };
