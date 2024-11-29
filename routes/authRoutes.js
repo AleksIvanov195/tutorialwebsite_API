1c1b843e7b606fdee830d1b4e69f4a4ad03eeaad
AleksIvanov195 <alexsandar196@gmail.com>
2024-11-29 13:46:56 +0000

    feat: authRoutes

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { validateCourse } from '../validators/courseValidator.js';
+import Accessor from '../accessors/Accessor.js';
+import AuthController from '../controllers/authController.js';
+import database from '../database.js';
+import userModel from '../models/userModel.js';
+const accessor = new Accessor(userModel, database);
+const controller = new AuthController(accessor);
+
+const router = express.Router();
+
+router.post('/register', (req, res) => controller.register(req, res));
+
+export default router;
