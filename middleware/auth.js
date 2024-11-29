7aa7a5627f0ff31c7230c8ddb19903d0b2b63098
AleksIvanov195 <alexsandar196@gmail.com>
2024-11-29 19:03:50 +0000

    feat: new auth function to restrict user type access to different endpoints

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,9 +12,21 @@ export const authenticateToken = (req, res, next) => {
 	try {
 		const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 		req.userID = decoded.userID;
+		req.userType = decoded.userType;
 		next();
 	} catch (error) {
 		return res.status(403).json({ message: 'Invalid access token' });
 	}
+};
+
+export const authoriseRoles = (roles = []) => (req, res, next) => {
+	if (roles.length === 0) {
+		return next();
+	}
+	if (!roles.includes(req.userType)) {
+		return res.status(403).json({ message: 'Forbidden: You do not have the required role to access this resource.' });
+	}
+
+	next();
 };
 
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { validateCourse } from '../validators/courseValidator.js';
-import { authenticateToken } from '../middleware/auth.js';
+import { authenticateToken, authoriseRoles } from '../middleware/auth.js';
 import Accessor from '../accessors/Accessor.js';
 import Controller from '../controllers/Controller.js';
 import database from '../database.js';
@@ -18,7 +18,7 @@ router.get('/:courseID', (req, res) => controller.get(req, res));
 router.get('/', (req, res) => controller.get(req, res));
 
 
-router.post('/', validateCourse, (req, res) => controller.post(req, res));
+router.post('/', validateCourse, authenticateToken, authoriseRoles(['Admin', 'ContentCreator']), (req, res) => controller.post(req, res));
 
 
 export default router;
