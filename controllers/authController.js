ee759677796894f02f29d2c1f18cf27c8aaf16b7
AleksIvanov195 <alexsandar196@gmail.com>
2024-11-29 18:25:50 +0000

    refactor: authController variables and comments

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,21 +7,31 @@ class AuthController {
 		this.accessor = accessor;
 	}
 	async register(req, res) {
+		// Data should contain user attributes
 		const data = req.body;
+
+		// Check if user exists
 		const existingUser = await this.accessor.fetchData(req);
 		if (existingUser && existingUser.length > 0) {
 			return res.status(400).json({ error: 'User already exists.' });
 		}
+
 		try{
+			// Hash the password
 			const hashedPassword = await bcrypt.hash(data.UserPassword, 10);
+
+			// Insert new user
 			const newUser = {
 				...data,
 				UserPassword: hashedPassword,
 			};
 			const { result } = await this.accessor.insertData(newUser);
 
+			// Generate jwt tokens
 			const accessToken = jwt.sign({ userID: result.insertId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
 			const refreshToken = jwt.sign({ userID: result.insertId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+
+			// Send message and tokens.
 			res.status(201).json({
 				message: 'Success Registering',
 				accessToken,
@@ -34,18 +44,27 @@ class AuthController {
 	}
 
 	async login(req, res) {
+		// Data should contain UserEmail and UserPassword
 		const data = req.body;
+
 		try{
-			const user = await this.accessor.fetchData(req);
-			if(!user) {
+			// Get user
+			const result = await this.accessor.fetchData(req);
+			if(!result) {
 				return res.status(404).json({ error: 'User not found.' });
 			}
-			const isPasswordValid = await bcrypt.compare(data.UserPassword, user[0].UserPassword);
+			const user = result[0];
+
+			// Verify password
+			const isPasswordValid = await bcrypt.compare(data.UserPassword, user.UserPassword);
 			if (!isPasswordValid) {
 				return res.status(401).json({ error: 'Incorrect password.' });
 			}
+			// Generate JWT tokens
 			const accessToken = jwt.sign({ userID: user.UserID }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
 			const refreshToken = jwt.sign({ userID: user.UserID }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+
+			// Send back message and the tokens
 			res.json({
 				message: 'Login successful.',
 				accessToken,
@@ -58,17 +77,21 @@ class AuthController {
 	}
 
 	async refresh(req, res) {
+		// Get Refresh token from req
 		const { refreshToken } = req.body;
 		try {
+
+			// Verify the refresh token
 			const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
 
-			const users = await this.accessor.fetchData({ body: { userID: decoded.userID } });
-			const user = users && users.length > 0 ? users[0] : null;
-
-			if (!user) {
+			// Get the user
+			const result = await this.accessor.fetchData({ body: { userID: decoded.userID } });
+			if (!result) {
 				return res.status(404).json({ error: 'User not found.' });
 			}
+			const user = result[0];
 
+			// Generate new access token
 			const newAccessToken = jwt.sign({ userID: user.UserID }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
 
 			res.json({ accessToken: newAccessToken });
