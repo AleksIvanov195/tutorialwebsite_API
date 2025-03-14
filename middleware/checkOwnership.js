7d2d259281da381ba49ad3acd794d4a7273118b1
AleksIvanov195 <alexsandar196@gmail.com>
2025-03-14 16:23:15 +0000

    fix: Major security issue where anyone will be able to delete your data. This is fixed by using the checkOwnership middleware - For example, if someone knows your usercourse ID they can delete or update it, checkOwnership will check the ownership the record ensuring that noone but the user can delete it.

---
+++ middleware/checkOwnership.js
@@ -0,0 +1,22 @@
+export const checkOwnership = (model, database) => async (req, res, next) => {
+	const userID = req.userID;
+	const recordID = req.params.id;
+	try {
+		// Fetch the record from the database
+		const query = `SELECT * FROM ${model.table} WHERE ${model.idField} = ?`;
+		const [record] = await database.execute(query, [recordID]);
+
+		if (!record) {
+			return res.status(404).json({ message: 'Record not found.' });
+		}
+
+		// Check if the user is the owner of the record
+		if (record[model.creatorField] !== userID) {
+			return res.status(403).json({ message: 'Forbidden: You do not have permission to access this resource.' });
+		}
+		next();
+	} catch (error) {
+		console.error('Error checking ownership: ', error);
+		res.status(500).json({ message: 'Internal Server Error: Ownership check failed.' });
+	}
+};
