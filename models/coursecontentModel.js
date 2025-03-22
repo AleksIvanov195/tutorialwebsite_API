5aefab3def629dffc1e4df80dbb0541297948d79
AleksIvanov195 <alexsandar196@gmail.com>
2025-03-22 15:22:22 +0000

    feat: update course content model to return true or false if the lesson is completed.

--- models/coursecontentModel.js
+++ models/coursecontentModel.js
@@ -35,27 +35,33 @@ const CourseContentModel = {
 				'CoursecontentOrder',
 				'CASE WHEN CoursecontentLessonID IS NOT NULL THEN CoursecontentLessonID ELSE CoursecontentQuizID END AS ContentID',
 				'CASE WHEN CoursecontentLessonID IS NOT NULL THEN "Lesson" ELSE "Quiz" END AS ContentType',
-				'COALESCE(Lesson.LessonName, Quiz.QuizName) AS ContentName',
+				'COALESCE(LessonName, QuizName) AS ContentName',
 			];
 
 			table += `
-			LEFT JOIN Lesson ON Coursecontent.CoursecontentLessonID = Lesson.LessonID
-			LEFT JOIN Quiz ON Coursecontent.CoursecontentQuizID = Quiz.QuizID`;
+			LEFT JOIN Lesson ON CoursecontentLessonID = LessonID
+			LEFT JOIN Quiz ON CoursecontentQuizID = QuizID`;
 		}
 
 		if (req.path.includes('/simplified/user-completion')) {
 			const userID = req.userID;
 			fields.push(
 				`CASE 
-        WHEN Coursecontent.CoursecontentQuizID IS NOT NULL THEN
+        WHEN CoursecontentQuizID IS NOT NULL THEN
             CASE 
-                WHEN MAX(Userquiz.UserquizUsercontentstatusID) = 3 THEN TRUE
+                WHEN MAX(UserquizUsercontentstatusID) = 3 THEN TRUE
                 ELSE FALSE
             END
+				WHEN CoursecontentLessonID IS NOT NULL THEN
+					CASE 
+							WHEN UserlessonUsercontentstatusID = 3 THEN TRUE
+							ELSE FALSE
+					END
    			 END AS ContentStatus`,
 			);
 			table += `
-			LEFT JOIN Userquiz ON Userquiz.UserquizQuizID = Quiz.QuizID AND Userquiz.UserquizUserID = :UserID
+			LEFT JOIN Userquiz ON UserquizQuizID = QuizID AND UserquizUserID = :UserID
+			LEFT JOIN Userlesson ON UserlessonLessonID = LessonID AND UserlessonUserID = :UserID
 			`;
 			groupBy = 'CoursecontentID';
 			parameters.UserID = parseInt(userID);
